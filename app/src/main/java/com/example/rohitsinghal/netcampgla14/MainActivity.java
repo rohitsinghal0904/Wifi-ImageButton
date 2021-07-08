505c425483be425213c42d0d4fb2f9f531078c43
Rohit Singhal <rohitsinghal0904@gmail.com>
2021-07-08 12:36:52 +0530

    this is a first commit

---
+++ app/src/main/java/com/example/rohitsinghal/netcampgla14/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.rohitsinghal.netcampgla14;
+
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton ib;
+    WifiManager wm;
+    private boolean wifi=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ib=(ImageButton)findViewById(R.id.imageButton);
+        wm=(WifiManager)getSystemService(WIFI_SERVICE);
+        ib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(wifi == false){
+                    wm.setWifiEnabled(true);
+                    wifi=true;
+                    ib.setImageResource(R.drawable.g);
+                }
+                else{
+                    wm.setWifiEnabled(false);
+                    wifi=false;
+                    ib.setImageResource(R.drawable.r);
+                }
+            }
+        });
+    }
+}
